d96763e5679f330ba3fa84cb81264f350bea4c95
unknown <satyrikonme@gmail.com>
2024-08-23 23:32:28 +1000

    basic changes

--- src/app/challenge_2/page.tsx
+++ src/app/challenge_2/page.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const page = () => {
   return (
-    <div className="flex flex-col  h-screen items-center justify-center bg-customyellow font-figtree">
-      <div className="flex flex-col bg-white w-2/3 h-auto m-4 rounded-xl border-black border-2 [box-shadow:5px_5px_0px_0px]  p-4 max-w-sm ">
+    <div className="flex flex-col h-screen items-center justify-center bg-customyellow font-figtree">
+      <div className="flex flex-col bg-white w-2/3 h-auto m-4 rounded-2xl border-black border-2 [box-shadow:4px_4px_0px_0px]  p-6 max-w-sm ">
         <img
           src="/images/illustration-article.svg"
           alt="illustration"
@@ -20,17 +20,28 @@ const page = () => {
           content, and presentation.
         </p>
         <div className="flex flex-row gap-2">
-          <img src="/images/image-avatar.webp" alt="" className="w-6 h-7" />
+          <img src="/images/image-avatar.webp" alt="" className="w-8 h-8" />
           <p className="font-bold"> Greg Hooper</p>
         </div>
       </div>
 
       <div className="text-xs mt-4">
-        Challenge by{" "}
-        <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
-          Frontend Mentor
-        </a>
-        . Coded by <a href="#">Prabin Maharjan</a>.
+        <p className="text-xs mt-4 text-center ">
+          Challenge by{" "}
+          <span className="text-blue-700 underline ">
+            <a
+              href="https:/www.frontendmentor.io?ref=challenge"
+              target="_blank"
+            >
+              Frontend Mentor
+            </a>
+          </span>
+          . Coded by{" "}
+          <span className="text-blue-700 underline">
+            <a href="https://'portfolio-prabin-vercel.app">Prabin Maharjan</a>
+          </span>
+          .
+        </p>
       </div>
     </div>
   );
